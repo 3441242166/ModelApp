25ee379d9bc6de3fb53332d86e9d9ea02012aab9
wanhao <wanhao>
2019-05-20 20:42:42 +0800

    添加了仿不背单词主界面layout和滑动冲突ViewPager

---
+++ myview/src/main/java/com/example/myview/threelayout/State.java
@@ -0,0 +1,7 @@
+package com.example.myview.threelayout;
+
+public class State {
+    public final static int TOP = 0;
+    public final static int CENTER = 1;
+    public final static int BOTTOM = 2;
+}
